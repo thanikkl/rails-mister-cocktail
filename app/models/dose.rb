f1ab8a9ecb6809e65211f3e4d27d630a8b7653bb
Thanh-An Le <lethanhannicolas@gmail.com>
2020-02-13 12:21:46 +0100

    model

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,5 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :description, presence: true
   validates :ingredient, :cocktail, presence: true
-  validates_uniqueness_of :ingredient, scope: :cocktail
+  validates :ingredient, uniqueness: { scope: :cocktail }
 end
